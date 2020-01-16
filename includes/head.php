e5efad9c83f6e45249381059359438087060fc01
Dhruba Rai <50292693+Rimpahang@users.noreply.github.com>
2020-01-17 01:15:42 +0545

    head file include

---
+++ includes/head.php
@@ -0,0 +1,20 @@
+<head>
+	<title>Khwopa Project Archive</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<script src="bootstrap/js/jquery.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+
+	<style>
+	.jumbotron{
+		height: 3%;
+		padding-top: 20px;
+		padding-bottom: 5px;
+	}
+	.navbar{
+		height: 80px;
+	}
+</style>
+</head>
