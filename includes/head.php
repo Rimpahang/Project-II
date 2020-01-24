c87173586b2ded3ebe637dcbb6eb6ec5c53f115c
Avishek-karki <50293963+Avishek-karki@users.noreply.github.com>
2020-01-24 21:17:36 +0545

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -8,13 +8,67 @@
 	<script src="bootstrap/js/bootstrap.min.js"></script>
 
 	<style>
-	.jumbotron{
-		height: 3%;
-		padding-top: 20px;
-		padding-bottom: 5px;
-	}
-	.navbar{
-		height: 80px;
-	}
+	.fakeimg {
+  		height: 200px;
+  		background: #aaa;
+  	}
+  	.jumbotron{
+  		height: 3%;
+  		padding-top: 20px;
+  		padding-bottom: 5px;
+  	}
+  	.navbar{
+  		/*height: 60px;*/
+  		text-transform: uppercase;
+  		font-weight: 700;
+  		font-size: .9rem;
+  		letter-spacing: .1rem;
+  		/*background: rgba(0, 0, 0, .1)!important;*/
+  	}
+  	.dropdown:hover>.dropdown-menu{
+  		display: block;
+  		background: #e3e3e3;
+  	}
+  	.container1{
+  		margin: 4% ;
+  	}
+  	#icon
+  	{
+  		max-width: 200px;
+  		margin: 5% auto;
+  	}
+  	.zoom{
+  		transition:transform .2s;
+  	}
+  	.zoom:hover{
+  		transform: scale(1.1); /* IE 9 */
+  		transform: scale(1.1); /* Safari 3-8 */
+  		transform: scale(1.1); 
+  	}
+  	hr.hr1{
+  		border: 1px light green;
+  	}
+
+  	footer{
+  		width: 100%;
+  		background-color: rgba(120,120,120);
+  		padding: 1%;
+  		color: #fff;
+  	}
+  	.fa{
+  		padding: 15px;
+  		font-size: 25px;
+  		color: #fff;
+  	}
+  	.fa:hover{
+  		color: orange;
+  		text-decoration: none;
+  	}
+  	@media(max-width: 900px){
+  		.fa{
+  			font-size: 20px;
+  			padding: 10px;
+  		}
+  	}
 </style>
 </head>
