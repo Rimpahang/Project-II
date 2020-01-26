e88fb75df515cf748dd2f7286c42a55311298a27
Rimpahang <rimpahang@gmail.com>
2020-01-26 23:16:37 +0545

    registration request popup

--- includes/head.php
+++ includes/head.php
@@ -2,7 +2,7 @@
 	<title>Khwopa Project Archive</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 	<script src="bootstrap/js/jquery.min.js"></script>
 	<script src="bootstrap/js/bootstrap.min.js"></script>
@@ -71,5 +71,8 @@
   			padding: 10px;
   		}
   	}
+        #reg-cross:hover{
+            cursor: pointer;
+        }
 </style>
 </head>
