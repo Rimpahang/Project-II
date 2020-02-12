d9a968c95e38aaa96c684a52d2f650b202329c79
Rimpahang <rimpahang@gmail.com>
2020-02-12 20:00:55 +0545

    mail system

---
+++ backEnd/password_recovery.php
@@ -0,0 +1,75 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+if (isset($_POST['sub'])) {
+    $email = $_POST['email'];
+
+//generate recovery key
+    $rekey = rand(100000,999999);
+
+
+// Load Composer's autoloader
+require 'phpmailer/vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'khwopaprojectarchive@gmail.com';                     // SMTP username
+    $mail->Password   = 'acdckanxoace';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('khwopaprojectarchive@gmail.com', 'Khwopa Project Archive');
+    $mail->addAddress($email, 'User');     // Add a recipient
+    // $mail->addAddress('ellen@example.com');               // Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
+
+    // Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Password Recovery';
+    $mail->Body    = 'Dear, User your recovery key is:<br><b>' . $rekey . '</b><br>Please do not share this recovery key with others.';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Recovery - Khwopa Project Archive</title>
+    <?php include('includes/head.php'); ?>
+</head>
+<body>
+    <div class="container-fluid col-md-5 bg-success">
+        Forgot your password?<br>Provide your email to send recover your account.
+        <form method="POST" name="precovery" action="">
+            <div class="form-group">
+                <label for="email">Enter Email Address</label>
+                <input type="email" name="email" required="required">
+            </div>
+            <input type="submit" name="sub" value="Send Recovery Code">
+        </form>
+    </div>
+
+</body>
+</html>
