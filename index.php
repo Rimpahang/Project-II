b37b085a657b893a685ac1dec763418f537dc6ae
Rimpahang <rimpahang@gmail.com>
2020-03-17 02:50:49 +0545

    frontend and backedn connected

--- index.php
+++ index.php
@@ -1,4 +1,78 @@
 <?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+if (isset($_POST['sub1'])) {
+    $email = $_POST['email1'];
+
+require_once('backEnd/includes/DBconnect.php');
+$validate_email_sql = "SELECT * FROM `kpa_user` WHERE `email` = '$email' AND `status` = '1'";
+$result = mysqli_query($conn, $validate_email_sql);
+if (mysqli_num_rows($result) > 0) {
+//generate recovery key
+    $rekey = rand(100000, 999999);
+    date_default_timezone_set('Asia/Kathmandu');
+    $sent_date = strtotime(date('Y-m-d H:i:s'));
+
+    $pwd_recovery_key_sql = "INSERT INTO `kpa_pwd_reset` (`email`, `reset_key`,`sent_time`) VALUES ('$email', '$rekey', '$sent_date')";
+
+    if (mysqli_query($conn, $pwd_recovery_key_sql)) {
+// Load Composer's autoloader
+        require 'backEnd/phpmailer/vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+        $mail = new PHPMailer(true);
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = 0;                      // Enable verbose debug output
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host = 'smtp.gmail.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth = true;                                   // Enable SMTP authentication
+            $mail->Username = 'khwopaprojectarchive@gmail.com';                     // SMTP username
+            $mail->Password = 'acdckanxoace';                               // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            $mail->Port = 587;                                    // TCP port to connect to
+
+            //Recipients
+            $mail->setFrom('khwopaprojectarchive@gmail.com', 'Khwopa Project Archive');
+            $mail->addAddress($email, 'User');     // Add a recipient
+            // $mail->addAddress('ellen@example.com');               // Name is optional
+            // $mail->addReplyTo('info@example.com', 'Information');
+            // $mail->addCC('cc@example.com');
+            // $mail->addBCC('bcc@example.com');
+
+            // Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Password Reset';
+            $mail->Body = 'Dear, User your password reset key is:<br><b>' . $rekey . '</b><br>Please do not share this key with anyone.';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo 'Message has been sent';
+
+            //getting id from db to sent to another page
+            $get_id_sql = "SELECT * FROM `kpa_pwd_reset` WHERE `email` = '$email' AND `status` = 1";
+            $pwd_reset_data = mysqli_query($conn, $get_id_sql);
+            $data = mysqli_fetch_assoc($pwd_reset_data);
+            header("location: backEnd/verify_pwd_reset_key.php?id=$data[id]");
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    } else
+        die("Error") . mysqli_error();
+}else
+    echo("No such email is registered to us!");
+}
+?>
+
+<?php
 require_once("backEnd/includes/DBconnect.php");
 $get_data_sql = "SELECT * FROM `kpa_project_list`";
 $result_data = mysqli_query($conn, $get_data_sql);
@@ -135,16 +209,19 @@ mysqli_close($conn);
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
           <h4 class="modal-title"><span class="glyphicon glyphicon-lock"></span> Recover Password!</h4>
         </div>
-        
+
+
+
+
         <div class="modal-body">
           <form method="post" id="Forgot-Password-Form" role="form">
             <div class="form-group">
                 <div class="input-group">
                 <div class="input-group-addon"><icon class="glyphicon glyphicon-envelope"></icon></div>
-                <input name="email" id="email3" type="email" class="form-control input-lg" placeholder="Enter Email" required data-parsley-type="email">
+                <input name="email1" id="email3" type="email" class="form-control input-lg" placeholder="Enter Email" required data-parsley-type="email">
                 </div>                     
             </div>         
-            <button type="submit" class="btn btn-success btn-block btn-lg">
+            <button type="submit" name="sub1" class="btn btn-success btn-block btn-lg">
               <span class="glyphicon glyphicon-send"></span> SUBMIT
             </button>
           </form>
